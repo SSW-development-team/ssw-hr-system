c853ca9d60c2d68c4cfebecf044e342bdc59ff60
volgakurvar <kurvan1112@gmail.com>
2022-05-31 00:33:15 +0900

    編集可能カラムコンポーネントのサンプルを追加

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -63,6 +63,32 @@ function App() {
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
     useTable({ columns, data });
 
+  const defaultColumn: Partial<ColumnDef<MyTableGenerics>> = {
+    cell: ({ getValue, row: { index }, column: { id }, instance }) => {
+      const initialValue = getValue();
+      // We need to keep and update the state of the cell normally
+      const [value, setValue] = React.useState(initialValue);
+
+      // When the input is blurred, we'll call our table meta's updateData function
+      const onBlur = () => {
+        instance.options.meta?.updateData(index, id, value);
+      };
+
+      // If the initialValue is changed external, sync it up with our state
+      React.useEffect(() => {
+        setValue(initialValue);
+      }, [initialValue]);
+
+      return (
+        <input
+          value={value as string}
+          onChange={(e) => setValue(e.target.value)}
+          onBlur={onBlur}
+        />
+      );
+    },
+  };
+
   return (
     <Container className="pt-5">
       <h1>SSW 人事管理システム</h1>
