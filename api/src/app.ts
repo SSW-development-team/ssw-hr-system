26726d8ce83e94a31bd5fd0135d74e2ed56bd149
volgakurvar <kurvan1112@gmail.com>
2022-06-01 23:34:50 +0900

    環境変数の読み込みに失敗していたのを修正

--- api/src/app.ts
+++ api/src/app.ts
@@ -45,7 +45,7 @@ export const AppDataSource = new DataSource({
   type: 'mysql',
   host: process.env.DB_HOST,
   port: 3306,
-  username: process.env.DB_USENAME,
+  username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: 'ssw_hr',
   entities: [User, Department],
