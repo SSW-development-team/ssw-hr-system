acfa2d411423ac99adcdb93fa333fbd818c4e937
volgakurvar <kurvan1112@gmail.com>
2022-07-13 23:47:50 +0900

    eslintルールを削除

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -18,7 +18,5 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['react', '@typescript-eslint'],
-  rules: {
-    '@typescript-eslint/prefer-nullish-coalescing': 'error',
-  },
+  rules: {},
 };
