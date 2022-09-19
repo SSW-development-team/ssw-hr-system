812ed82a63fea198eed8d793e72651bddbe2e63a
volgakurvar <kurvan1112@gmail.com>
2022-09-19 16:35:45 +0900

    ツリーノードカードを追加

--- front/src/pages/OrgChart/Card.tsx
+++ front/src/pages/OrgChart/Card.tsx
@@ -1,4 +1,24 @@
+import { Avatar, Grid, Paper, Stack, Typography } from '@mui/material';
 
-export default function Card(name: string) {
-  return <h1>name</h1>;
+export default function Card(
+  rolename: string,
+  user: { name: string; img?: string }
+) {
+  return (
+    <Paper>
+      <Grid container spacing={2}>
+        <Grid xs={4}>
+          <Avatar
+            src={user.img ?? 'https://mui.com/static/images/avatar/1.jpg'}
+          />
+        </Grid>
+        <Grid xs={8}>
+          <Stack>
+            <Typography>{user.name}</Typography>
+            <Typography>{rolename}</Typography>
+          </Stack>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
 }
