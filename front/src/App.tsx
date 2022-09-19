c650bb86b35a2fe0e354ee6c489f5c837b331e2a
volgakurvar <kurvan1112@gmail.com>
2022-09-19 15:18:39 +0900

    ナビゲーションバーを追加

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,15 +1,45 @@
 /* eslint-disable react/jsx-key */
 import React from 'react';
 import './App.css';
-import { Container } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+  Container,
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function App(): JSX.Element {
   return (
-    <Container className="pt-5" fluid>
-      <h1>SSW 人事管理システム3</h1>
-      <Outlet />
-    </Container>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              SSW 人事管理システム
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Container>
+        <h1>SSW 人事管理システム3</h1>
+        <Outlet />
+      </Container>
+    </>
   );
 }
 
